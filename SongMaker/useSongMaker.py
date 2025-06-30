fc326d5cf9e853297ccff7e35905333a6eea7bae
Sim-JuHeun <juheun9912@gmail.com>
2025-06-30 13:10:54 +0900

    기타 트랙 화음으로 구성하기

--- SongMaker/useSongMaker.py
+++ SongMaker/useSongMaker.py
@@ -1,26 +1,32 @@
-from ai_song_maker.score_helper import process_and_output_score
+from music21 import instrument
 
-# 코드 진행
-predicted_chords = ["C", "G", "Am", "F", "C", "G", "F", "C"]
-
-# 코드별 4분음표 아르페지오 패턴
-chord_patterns = {
+# 코드별 아르페지오(피아노)와 코드톤(기타)
+chord_patterns_piano = {
     "C": ["C4", "E4", "G4", "E4"],
     "G": ["G3", "B3", "D4", "B3"],
     "Am": ["A3", "C4", "E4", "C4"],
     "F": ["F3", "A3", "C4", "A3"]
 }
 
+chord_patterns_guitar = {
+    "C": ["C3", "E3", "G3"],
+    "G": ["G3", "B3", "D4"],
+    "Am": ["A3", "C4", "E4"],
+    "F": ["F3", "A3", "C4"]
+}
+
+predicted_chords = ["C", "G", "Am", "F", "C", "G", "F", "C"]
+
 parts_data = {
     "Piano": {
-        "instrument": "Piano",
+        "instrument": instrument.Piano(),
         "melodies": [],
         "beat_ends": [],
         "dynamics": [],
         "lyrics": []
     },
     "Guitar": {
-        "instrument": "AcousticGuitar",  # 또는 "AcousticGuitar"
+        "instrument": instrument.AcousticGuitar(),
         "melodies": [],
         "beat_ends": [],
         "dynamics": [],
@@ -30,21 +36,24 @@ parts_data = {
 
 current_beat = 0.0
 for chord in predicted_chords:
-    notes = chord_patterns.get(chord, ["C4", "E4", "G4", "E4"])
-    for note_name in notes:
-        duration = 1.0  # 4분음표
-        # Piano
+    # 피아노: 아르페지오 (4분음표 4개)
+    piano_notes = chord_patterns_piano.get(chord, ["C4", "E4", "G4", "E4"])
+    for note_name in piano_notes:
+        duration = 1.0
         parts_data["Piano"]["melodies"].append(note_name)
-        # Guitar (같은 음)
-        parts_data["Guitar"]["melodies"].append(note_name)
         current_beat += duration
         parts_data["Piano"]["beat_ends"].append(current_beat)
-        parts_data["Guitar"]["beat_ends"].append(current_beat)
         parts_data["Piano"]["dynamics"].append("mf")
-        parts_data["Guitar"]["dynamics"].append("mf")
         parts_data["Piano"]["lyrics"].append("")
-        parts_data["Guitar"]["lyrics"].append("")
+    # 기타: 화음(코드) (한 마디 길이만큼)
+    guitar_chord = chord_patterns_guitar.get(chord, ["C3", "E3", "G3"])
+    # 한 마디의 마지막 beat_end에 맞춰서
+    parts_data["Guitar"]["melodies"].append(guitar_chord)  # 리스트로!
+    parts_data["Guitar"]["beat_ends"].append(current_beat)
+    parts_data["Guitar"]["dynamics"].append("mf")
+    parts_data["Guitar"]["lyrics"].append("")
 
+# 기본 정보
 score_data = {
     'key': 'C',
     'time_signature': '4/4',
@@ -52,8 +61,11 @@ score_data = {
     'clef': 'treble'
 }
 
+# 파일 경로
 output_musicxml_path = "/Users/simjuheun/Desktop/myProject/New_LSTM/LSTM/cli/data/rock_midi/rock_sample.xml"
 output_midi_path = "/Users/simjuheun/Desktop/myProject/New_LSTM/LSTM/cli/data/rock_midi/rock_sample.mid"
+
+from ai_song_maker.score_helper import process_and_output_score
 
 process_and_output_score(
     parts_data,
